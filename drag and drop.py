96760b2f2434868468a70bdee8dc4c73d76a3e90
Qwabena <100248770+Qwabena-Proxy@users.noreply.github.com>
2022-04-28 12:13:04 +0000

    Add files via upload

---
+++ drag and drop.py
@@ -0,0 +1,30 @@
+from tkinter import *
+
+def drag_start(event):
+	widget = event.widget
+	widget.startX = event.x
+	widget.startY = event.y
+
+def drag_motion(event):
+	widget = event.widget
+	x = widget.winfo_x() - widget.startX + event.x
+	y = widget.winfo_y() - widget.startY + event.y
+	widget.place(x = x , y = y)
+
+window = Tk()
+
+red_label = Label(window,bg="red",width = 10, height=5)
+red_label.place(x=0,y=0)
+
+red_label.bind("<Button-1>",drag_start)
+red_label.bind("<B1-Motion>",drag_motion)
+
+blue_label = Label(window,bg="blue",width = 10, height=5)
+blue_label.place(x = 100,y = 100)
+
+blue_label.bind("<Button-1>",drag_start)
+blue_label.bind("<B1-Motion>",drag_motion)
+
+
+
+window.mainloop()         
